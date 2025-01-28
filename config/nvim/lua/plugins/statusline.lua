04f63bbfb08735dae82957acc7136df3bb34fbb0
Gustav Behm <me@rootmos.io>
2025-01-28 19:43:07 +0100

    Remove spell from airline

--- config/nvim/lua/plugins/statusline.lua
+++ config/nvim/lua/plugins/statusline.lua
@@ -7,6 +7,14 @@ return {
     config = function()
       vim.api.nvim_set_var("airline_solarized_bg", "dark")
       vim.api.nvim_set_var("airline_theme", "solarized")
+
+      vim.api.nvim_set_var("airline_section_a",
+        vim.fn["airline#section#create_left"]{
+          -- https://github.com/vim-airline/vim-airline/blob/7a552f415c48aed33bf7eaa3c50e78504d417913/autoload/airline/init.vim#L251
+          -- "mode", "crypt", "paste", "keymap", "spell", "capslock", "xkblayout", "iminsert",
+          "mode", "crypt", "paste", "keymap", "capslock", "xkblayout", "iminsert",
+        }
+      )
     end,
   }
 }
