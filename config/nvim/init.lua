8ca87ebd3c09c0005328f5ff388046ce6ba9d727
Gustav Behm <me@rootmos.io>
2025-01-28 19:31:35 +0100

    Move the filetype settings into its own file

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -40,33 +40,4 @@ end
 resetTabs(4)
 
 require("config.lazy")
-
-vim.g.do_filetype_lua = 1
-vim.filetype.add {
-    filename = {
-        [".k"] = "sh",
-    },
-    pattern = {
-        [".*%.celx"] = "lua",
-    },
-}
-
-vim.api.nvim_create_autocmd("BufNewFile", {
-    pattern = "*.tf",
-    callback = function()
-        vim.bo.filetype = "terraform"
-    end,
-})
-
-vim.keymap.set("n", "<leader>d", function() vim.api.nvim_command(":filetype detect") end)
-
-vim.api.nvim_create_autocmd("FileType", {
-    pattern = {
-        "mail", "gitcommit", "tex", "markdown",
-        "c", "python", "haskell", "lua", "go",
-    },
-    callback = function()
-        vim.opt_local.spell = true
-        vim.opt_local.spelllang = "en,sv"
-    end,
-})
+require("filetypes")
