75afa1e9336f24a87d87de340b93057f8552ce71
Gustav Behm <me@rootmos.io>
2025-01-29 07:27:28 +0100

    Simplify airline's section Z

--- config/nvim/lua/plugins/statusline.lua
+++ config/nvim/lua/plugins/statusline.lua
@@ -9,12 +9,10 @@ return {
       vim.api.nvim_set_var("airline_theme", "solarized")
 
       vim.api.nvim_set_var("airline_section_a",
-        vim.fn["airline#section#create_left"]{
-          -- https://github.com/vim-airline/vim-airline/blob/7a552f415c48aed33bf7eaa3c50e78504d417913/autoload/airline/init.vim#L251
-          -- "mode", "crypt", "paste", "keymap", "spell", "capslock", "xkblayout", "iminsert",
-          "mode", "crypt", "paste", "keymap", "capslock", "xkblayout", "iminsert",
-        }
+        vim.fn["airline#section#create_left"]{ "mode", "paste", }
       )
+
+      vim.api.nvim_set_var("airline_section_z", "%P %l/%L:%v")
     end,
   }
 }
