c3af2c228bd52aa5cb0c5b810aa55a9dc67ce3c4
Gustav Behm <me@rootmos.io>
2025-01-30 17:20:38 +0100

    Wrap should be set regardless of longlineedness

--- config/nvim/ftplugin/mail.lua
+++ config/nvim/ftplugin/mail.lua
@@ -1,18 +1,18 @@
 vim.opt_local.linebreak = true
 vim.opt_local.formatoptions:append("lj")
 vim.opt_local.joinspaces = false
+vim.opt_local.wrap = true
 
-function toggleWrap()
-    if vim.opt_local.wrap:get() then
+local longLines = true -- i.e. default is short lines
+function toggleLongLines()
+    longLines = not longLines
+    if longLines then
         vim.opt_local.textwidth = 1073741824
-        vim.opt_local.wrap = false
         print("long lines")
     else
         vim.opt_local.textwidth = 72
-        vim.opt_local.wrap = true
-        print("wrapped lines")
+        print("short lines")
     end
 end
-toggleWrap()
-
-vim.keymap.set("n", "<leader>w", toggleWrap)
+toggleLongLines()
+vim.keymap.set("n", "<leader>l", toggleLongLines)
