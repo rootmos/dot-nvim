65e5b2fa84c51d0b66f029d822e5d107f80e83a1
Gustav Behm <me@rootmos.io>
2025-02-15 15:23:09 +0100

    Disable vim-airline

--- config/nvim/lua/plugins/statusline.lua
+++ config/nvim/lua/plugins/statusline.lua
@@ -1,18 +1,20 @@
-return {
-  {
-    "vim-airline/vim-airline-themes",
-    dependencies = {
-      { "vim-airline/vim-airline" },
-    },
-    config = function()
-      vim.api.nvim_set_var("airline_solarized_bg", "dark")
-      vim.api.nvim_set_var("airline_theme", "solarized")
+return {}
 
-      vim.api.nvim_set_var("airline_section_a",
-        vim.fn["airline#section#create_left"]{ "mode", "paste", }
-      )
+--return {
+  --{
+    --"vim-airline/vim-airline-themes",
+    --dependencies = {
+      --{ "vim-airline/vim-airline" },
+    --},
+    --config = function()
+      --vim.api.nvim_set_var("airline_solarized_bg", "dark")
+      --vim.api.nvim_set_var("airline_theme", "solarized")
 
-      vim.api.nvim_set_var("airline_section_z", "%P %l/%L:%v")
-    end,
-  }
-}
+      --vim.api.nvim_set_var("airline_section_a",
+        --vim.fn["airline#section#create_left"]{ "mode", "paste", }
+      --)
+
+      --vim.api.nvim_set_var("airline_section_z", "%P %l/%L:%v")
+    --end,
+  --}
+--}
