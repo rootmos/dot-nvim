a9ee524d466e78efc2444dfc82b572c606545359
Gustav Behm <me@rootmos.io>
2025-02-16 10:51:06 +0100

    Let's try nvim-lualine

--- config/nvim/lua/plugins/statusline.lua
+++ config/nvim/lua/plugins/statusline.lua
@@ -1,20 +1,15 @@
-return {}
-
---return {
-  --{
-    --"vim-airline/vim-airline-themes",
-    --dependencies = {
-      --{ "vim-airline/vim-airline" },
-    --},
-    --config = function()
-      --vim.api.nvim_set_var("airline_solarized_bg", "dark")
-      --vim.api.nvim_set_var("airline_theme", "solarized")
-
-      --vim.api.nvim_set_var("airline_section_a",
-        --vim.fn["airline#section#create_left"]{ "mode", "paste", }
-      --)
-
-      --vim.api.nvim_set_var("airline_section_z", "%P %l/%L:%v")
-    --end,
-  --}
---}
+return {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+        require("lualine").setup {
+            options = {
+                -- https://github.com/nvim-lualine/lualine.nvim/blob/master/THEMES.md
+                theme = "solarized_dark",
+            },
+            sections = {
+                lualine_x = { "encoding", "filetype" },
+            },
+        }
+    end,
+}
