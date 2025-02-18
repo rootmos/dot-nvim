a5a87ded51f5216a559101a7f27899ec7975d3bc
Gustav Behm <me@rootmos.io>
2025-02-18 06:17:24 +0100

    Add two datetime related snippets

--- config/nvim/lua/snippets.lua
+++ config/nvim/lua/snippets.lua
@@ -109,6 +109,12 @@ ls.add_snippets("python", {
     s("script_dir", {
         t{"script_dir = os.path.dirname(os.path.realpath(__file__))", ""},
     }),
+    s("now", {
+        t{"datetime.datetime.now(datetime.UTC)"},
+    }),
+    s("iso8601", {
+        t{'isoformat(timespec="seconds")'},
+    }),
 })
 
 do
