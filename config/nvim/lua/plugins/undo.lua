f5884d088227e6275b798ecf13b4c5b80cbec7e1
Gustav Behm <me@rootmos.io>
2025-02-26 09:18:59 +0100

    Try out an undotree plugin

---
+++ config/nvim/lua/plugins/undo.lua
@@ -0,0 +1,11 @@
+return {
+    {
+        "mbbill/undotree",
+        config = function()
+            vim.keymap.set("n", "<leader>u", function()
+                vim.cmd.UndotreeShow()
+                vim.cmd.UndotreeFocus()
+            end)
+        end,
+    },
+}
