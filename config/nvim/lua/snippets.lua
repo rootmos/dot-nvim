8aa2e2cf0288c231ec042445779ea6d7368683fb
Gustav Behm <me@rootmos.io>
2025-03-29 06:56:26 +0100

    Add a logging.getLogger(__name__) snippet

--- config/nvim/lua/snippets.lua
+++ config/nvim/lua/snippets.lua
@@ -42,7 +42,7 @@ ls.add_snippets("sh", {
     s("mktemp", {
         t{"TMP=$(mktemp -d)", "trap 'rm -rf $TMP' EXIT", "", ""},
     }),
-    s("SCRIPT_DIR", {
+    s("SCRIPT_DIR=", {
         t{'SCRIPT_DIR=$(readlink -f "$0" | xargs dirname)', ""},
     }),
     s("getopts", {
@@ -120,10 +120,14 @@ ls.add_snippets("python", {
         t{"script_dir = os.path.dirname(os.path.realpath(__file__))", ""},
     }),
     s("now", {
-        t{"datetime.datetime.now(datetime.UTC)"},
+        t{"datetime.now(datetime.UTC)"},
     }),
     s("iso8601", {
         t{'isoformat(timespec="seconds")'},
+    }),
+    s("logging", {
+        t{"import logging", ""},
+        t{"logger = logging.getLogger(__name__)"},
     }),
 })
 
