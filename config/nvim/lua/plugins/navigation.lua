893f60ff839c768ca1ea60ab7bfd596b0c577c38
Gustav Behm <me@rootmos.io>
2025-05-16 06:48:28 +0200

    Rework the buffer navigation

--- config/nvim/lua/plugins/navigation.lua
+++ config/nvim/lua/plugins/navigation.lua
@@ -67,7 +67,6 @@ return {
 
             vim.keymap.set("n", "<leader>t", builtin.git_files)
             vim.keymap.set("n", "<leader>f", builtin.find_files)
-            vim.keymap.set("n", "<leader>b", builtin.buffers)
             vim.keymap.set("n", "<leader>g", builtin.live_grep)
             vim.keymap.set("n", "<leader>G", builtin.grep_string)
             vim.keymap.set("n", "<leader>j", builtin.jumplist)
@@ -75,6 +74,14 @@ return {
             vim.keymap.set("n", "<leader>r", builtin.resume)
             vim.keymap.set("n", "<leader>k", builtin.man_pages)
             vim.keymap.set("n", "<leader>=", builtin.spell_suggest)
+
+            vim.keymap.set("n", "<leader>b", function()
+                builtin.buffers {
+                    ignore_current_buffer = true,
+                    sort_lastused = true,
+                    sort_mru = true,
+                }
+            end)
         end,
     },
 }
