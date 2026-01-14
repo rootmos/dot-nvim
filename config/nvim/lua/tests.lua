c0d8f1a6a78c286da2b1d8b359468bbb06d668d7
Gustav Behm <me@rootmos.io>
2026-01-14 06:40:15 +0100

    Make the testdir:s acceptable for the cache action

--- config/nvim/lua/tests.lua
+++ config/nvim/lua/tests.lua
@@ -1,7 +1,7 @@
 local test_case = DOT_NVIM_TESTS or ""
 if test_case == "sanity-check" then
     error("make sure errors are caught")
-elseif test_case:sub(0,3) == "ft:" then
+elseif test_case:sub(0,3) == "ft-" then
     local ft = test_case:sub(4)
 
     vim.cmd.enew()
