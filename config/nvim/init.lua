ecb29247349ad6377a17c94aca7e9a984057eacb
Gustav Behm <me@rootmos.io>
2026-01-14 05:51:38 +0100

    Override vim.notify and rework the test verdict

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -3,13 +3,21 @@ if not DOT_NVIM_TESTS then
     return require("main")
 end
 
-local result_path = os.getenv("DOT_NVIM_TESTS_RESULT_PATH")
-local f = io.open(result_path, "w")
+local f = io.open(os.getenv("DOT_NVIM_TESTS_RESULT_PATH"), "w")
 
 local function ts()
     local rt = vim.uv.clock_gettime("realtime")
     f:write(string.format("%d.%09d\n", rt["sec"], rt["nsec"]))
+    f:flush()
 end
+
+local messages = io.open(os.getenv("DOT_NVIM_TESTS_MESSAGES_PATH"), "w")
+vim.notify = function(msg, level, opts)
+    messages:write(msg)
+    messages:write("\n")
+    messages:flush()
+end
+vim.notify_once = vim.notify
 
 ts()
 local sc, msg = pcall(require, "main")
@@ -20,8 +28,8 @@ vim.schedule(function()
     f:write(string.format("%d\n", sc and 1 or 0))
     if not sc then
         f:write(msg)
+        f:flush()
     end
-    f:close()
 
     vim.cmd { cmd = sc and "quit" or "cquit" }
 end)
