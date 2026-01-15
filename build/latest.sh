70a56eb13e311af829c8a0d120d0c5ededa44a60
Gustav Behm <me@rootmos.io>
2026-01-15 04:52:55 +0100

    Add a workflow that checks the latest Neovim version

--- build/latest.sh
+++ build/latest.sh
@@ -6,11 +6,25 @@ SCRIPT_DIR=$(readlink -f "$0" | xargs dirname)
 ROOT=$(readlink -f "$SCRIPT_DIR/..")
 FETCH=$SCRIPT_DIR/fetch
 
+SKIP_BUILD=
+while getopts "B-" OPT; do
+    case $OPT in
+        B) SKIP_BUILD=1 ;;
+        -) break ;;
+        ?) exit 2 ;;
+    esac
+done
+shift $((OPTIND-1))
+
 CURRENT=$(<"$ROOT/version")
 LATEST=$(gh release list --exclude-pre-releases --repo neovim/neovim --json tagName --jq 'map(.tagName) | map(select(. != "stable")) | .[0]')
 
 echo 1>&2 "current version: v$CURRENT"
 echo 1>&2 "latest version: $LATEST"
+
+if [ "v$CURRENT" == "$LATEST" ]; then
+    exit 0
+fi
 
 if [ -z "${WORKDIR-}" ]; then
     WORKDIR=$(mktemp -d)
@@ -18,19 +32,24 @@ if [ -z "${WORKDIR-}" ]; then
 else
     mkdir -p "$WORKDIR"
 fi
-export WORKDIR=$(readlink -f "$WORKDIR")
+WORKDIR=$(readlink -f "$WORKDIR")
+export WORKDIR
 cd "$WORKDIR"
 
-if [ "v$CURRENT" != "$LATEST" ]; then
-    URL="https://github.com/neovim/neovim/archive/refs/tags/$LATEST.tar.gz"
-    TARBALL="nvim-$LATEST.tar.gz"
+URL="https://github.com/neovim/neovim/archive/refs/tags/$LATEST.tar.gz"
+TARBALL="nvim-$LATEST.tar.gz"
 
-    echo 1>&2 "fetching: $URL"
-    $FETCH --root="$WORKDIR" --manifest-filename="$SCRIPT_DIR/nvim.json" add "$URL" "$TARBALL"
+echo 1>&2 "fetching: $URL"
+$FETCH --root="$WORKDIR" --manifest-filename="$SCRIPT_DIR/nvim.json" add "$URL" "$TARBALL"
 
+if [ -z "$SKIP_BUILD" ]; then
     echo 1>&2 "installing: $LATEST"
     "$SCRIPT_DIR/install.sh" -v "$LATEST"
+fi
 
-    echo 1>&2 "set current version: v$LATEST"
-    echo "$LATEST" >"$ROOT/version"
+echo 1>&2 "set current version: v$LATEST"
+echo "$LATEST" >"$ROOT/version"
+
+if [ -z "$SKIP_BUILD" ]; then
+    exit 1
 fi
