b5ea4b31de98d031182dcee8dac1b7d111eab72e
Gustav Behm <me@rootmos.io>
2026-05-02 09:48:41 +0200

    Add a keybinding in tex-files to open the corresponding pdf

--- config/nvim/ftplugin/tex.lua
+++ config/nvim/ftplugin/tex.lua
@@ -1,2 +1,14 @@
 vim.keymap.set("i", '"', "\\enquote{", { buffer = true })
-vim.keymap.set("i", '*', "\\emph{", { buffer = true })
+vim.keymap.set("i", "*", "\\emph{", { buffer = true })
+
+local function za(t)
+    return function()
+        local fn = vim.fn.expand("%")
+        if fn:match("%.tex$") ~= nil then
+            local path = fn:sub(1,-4) .. t .. ".pdf"
+            vim.system({ "za", path }, { detached = true })
+        end
+    end
+end
+vim.keymap.set("n", "za", za("draft"), { buffer = true })
+vim.keymap.set("n", "zA", za("final"), { buffer = true })
