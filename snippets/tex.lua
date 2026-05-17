415f81363645b81f101b1c3bc523059be8d9c2a4
Gustav Behm <me@rootmos.io>
2026-05-17 08:48:36 +0200

    Tweak the \itemize snippet

--- snippets/tex.lua
+++ snippets/tex.lua
@@ -11,7 +11,7 @@ return {
         t{"    "}, i(2), t{"", ""},
         t{"\\end{"}, rep(1), t{"}"},
     }),
-    s("itemize", {
+    s("\\itemize", {
         t{"\\begin{itemize}", ""},
         t{"    \\item "}, i(1), t{"", ""},
         t{"\\end{itemize}", ""},
