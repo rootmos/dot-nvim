211b7dedf998c2a8714ae87e77e03c1db68609bb
Gustav Behm <me@rootmos.io>
2026-05-19 20:43:13 +0200

    Insert the \item if necessary

--- config/nvim/ftplugin/tex.lua
+++ config/nvim/ftplugin/tex.lua
@@ -14,3 +14,19 @@ vim.keymap.set("n", "za", za("draft"), { buffer = true })
 vim.keymap.set("n", "zA", za("final"), { buffer = true })
 
 vim.opt_local.iskeyword:append("+")
+
+local function itemize()
+    local pos = pos or vim.fn.getpos(".")
+    local buf, row, col, off = unpack(pos)
+    local ls = vim.api.nvim_buf_get_lines(buf, row-1, row, false)
+    local l = ls[1]
+
+    local _, i = vim.regex([[^\s*]]):match_str(l)
+    if not vim.startswith(l:sub(i+1), [[\item]]) then
+        vim.cmd([[keeppatterns .s/^\s*/\\item /]])
+        vim.fn.setpos(".", {buf, row, col - i + 6, off})
+    end
+end
+
+vim.keymap.set("i", ",i", itemize, { buffer = true })
+vim.keymap.set("n", ",i", itemize, { buffer = true })
