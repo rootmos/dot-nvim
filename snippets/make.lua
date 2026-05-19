58ff481adea2ac81e1d42610de22c34b235ddb4a
Gustav Behm <me@rootmos.io>
2026-05-19 07:25:18 +0200

    Add a THIS_DIR Makefile snippet

--- snippets/make.lua
+++ snippets/make.lua
@@ -2,6 +2,9 @@ return {
     s("CURRENT_DIR :=", {
         t{"CURRENT_DIR := $(shell dirname $(realpath $(firstword $(MAKEFILE_LIST))))", ""},
     }),
+    s("THIS_DIR :=", {
+        t{"THIS_DIR := $(shell dirname $(realpath $(lastword $(MAKEFILE_LIST))))", ""},
+    }),
     s(".PHONY", {
         t{".PHONY: "}, i(1), t{"", ""},
         rep(1), t{":", ""},
